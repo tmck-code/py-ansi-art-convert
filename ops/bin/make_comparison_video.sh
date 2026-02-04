6b5e8642c7576351a0833f3a32644c71e9534b3a
Tom McKeesick <tmck01@gmail.com>
2026-02-05 00:38:43 +1100

    slight tweaks to movie renderer

--- ops/bin/make_comparison_video.sh
+++ ops/bin/make_comparison_video.sh
@@ -4,21 +4,29 @@ set -euo pipefail
 
 artwork="$1"
 official_png="$2"
-time="${3:-60}"
+width="$3"
+time="${4:-60}"
 
 artwork_basename=$(basename "$artwork" .ans)
 
 rm -rf screenshots/
-./bin/scroll_and_screenshot.py "$artwork" demo.png
+./ops/bin/scroll_and_screenshot.py "$artwork" "$width" demo.png
+clear -x
 
-ffpb -loop 1 \
-  -y \
-  -i "$official_png" \
-  -vf scale=1440:-2,crop=1440:1980:0:"'(ih-oh)*t/$time'" \
-  -t "$time" \
-  -c:v libx264 \
-  -pix_fmt yuv420p \
-  "${artwork_basename}.16colors.mkv"
+if test -f "${artwork_basename}.16colors.mkv" \
+  && ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "${artwork_basename}.16colors.mkv" | grep -q "^${time}\.0*$"; then
+  echo "Using cached ${artwork_basename}.16colors.mkv"
+else
+  echo "generating ${artwork_basename}.16colors.mkv"
+  ffpb -loop 1 \
+    -y \
+    -i "$official_png" \
+    -vf scale=1440:-2,crop=1440:1980:0:"'(ih-oh)*t/$time'" \
+    -t "$time" \
+    -c:v libx264 \
+    -pix_fmt yuv420p \
+    "${artwork_basename}.16colors.mkv"
+fi
 
 ffpb -loop 1 \
   -y \
