8361e91013bd0e31ce4f5fed22bb32f806eb1714
Tom McKeesick <tmck01@gmail.com>
2026-02-06 08:19:09 +1100

    minor updates
    - use tuples rather than dicts for char lists
    - add missing import

--- ansi_art_convert/encoding.py
+++ ansi_art_convert/encoding.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 from enum import Enum
 from collections import Counter
 
+from laser_prynter import pp
+
 from ansi_art_convert.log import dprint, DEBUG
 
 class SupportedEncoding(Enum):
@@ -17,27 +19,27 @@ class SupportedEncoding(Enum):
                 return encoding
         raise ValueError(f'Unsupported encoding: {value}')
 
-CP437_SHADE_BLOCK_MAP = {
-    0xB0: '░',
-    0xB1: '▒',
-    0xB2: '█',
-}
-CP437_BLOCK_MAP = {
-    0xDB: '█',
-    0xDC: '▄',
-    0xDD: '▐',
-    0xDE: '▌',
-    0xDF: '▀',
-}
-CP437_BOX_MAP = {
-    0xC0: '└',
-    0xD9: '┘',
-    0xC3: '├',
-    0xC2: '┬',
-    0xC1: '┐',
-    0xB4: '┤',
-}
-CP437_DOUBLE_BOX_MAP = {
+CP437_SHADE_BLOCK_MAP = (
+    0xB0, # '░'
+    0xB1, # '▒'
+    0xB2, # '█'
+)
+CP437_BLOCK_MAP = (
+    0xDB, # '█'
+    0xDC, # '▄'
+    0xDD, # '▐'
+    0xDE, # '▌'
+    0xDF, # '▀'
+)
+CP437_BOX_MAP = (
+    0xC0, # '└'
+    0xD9, # '┘'
+    0xC3, # '├'
+    0xC2, # '┬'
+    0xC1, # '┐'
+    0xB4, # '┤'
+)
+CP437_DOUBLE_BOX_MAP = (
     0xB6, #╢
     0xB7, #╖
     0xB8, #╕
@@ -64,15 +66,14 @@ CP437_DOUBLE_BOX_MAP = {
     0xD6, #╓
     0xD7, #╫
     0xD8, #╪
-}
-
-ISO_8859_1_BOX_MAP = {
-    0x7c: '|',
-    0x5c: '\\',
-    0x2f: '/',
-    0xaf: '¯',
-    0x5f: '_',
-}
+)
+ISO_8859_1_BOX_MAP = (
+    0x7c, # '|'
+    0x5c, # '\\'
+    0x2f, # '/'
+    0xaf, # '¯'
+    0x5f, # '_'
+)
 POPULAR_CHAR_MAP = {
     'Ñ': {SupportedEncoding.CP437: 0xA5, SupportedEncoding.ISO_8859_1: 0xD1},
 }
@@ -114,16 +115,16 @@ def detect_encoding(data: bytes) -> SupportedEncoding:
             points[encoding] += odd_char['points']
 
     iso_box_counts = Counter()
-    for byte in ISO_8859_1_BOX_MAP.keys():
+    for byte in ISO_8859_1_BOX_MAP:
         iso_box_counts[byte] = data.count(byte)
 
     cp437_shade_counts, cp437_box_counts, cp437_block_counts = Counter(), Counter(), Counter()
 
     all_counts = (
         (CP437_SHADE_BLOCK_MAP, cp437_shade_counts),
-        (CP437_BOX_MAP, cp437_box_counts),
-        (CP437_BLOCK_MAP, cp437_block_counts),
-        (CP437_DOUBLE_BOX_MAP, cp437_block_counts)
+        (CP437_BOX_MAP,         cp437_box_counts),
+        (CP437_BLOCK_MAP,       cp437_block_counts),
+        (CP437_DOUBLE_BOX_MAP,  cp437_block_counts)
     )
 
     for bytes, counts in all_counts:
@@ -134,16 +135,19 @@ def detect_encoding(data: bytes) -> SupportedEncoding:
 
     for c in (cp437_shade_counts, cp437_box_counts, cp437_block_counts):
         if c.total() > 0:
-            dprint(f'> [CP437 +1] Detected CP437 characters in file: {c}')
+            if DEBUG:
+                dprint(f'> [CP437 +1] Detected CP437 characters in file: {c}')
             points[SupportedEncoding.CP437] += 1
 
     cp437_all_counts = cp437_shade_counts | cp437_box_counts | cp437_block_counts
     if len(cp437_all_counts) > 1:
         if cp437_all_counts.total() < iso_box_counts.total():
-            dprint(f'> [ISO +1] Detected more ISO-8859-1 box characters in file than CP437: {iso_box_counts.total()} vs {cp437_all_counts.total()}')
+            if DEBUG:
+                dprint(f'> [ISO +1] Detected more ISO-8859-1 box characters in file than CP437: {iso_box_counts.total()} vs {cp437_all_counts.total()}')
             points[SupportedEncoding.ISO_8859_1] += 1
         else:
-            dprint(f'> [CP437 +1] Detected CP437 characters in file: {counts}')
+            if DEBUG:
+                dprint(f'> [CP437 +1] Detected CP437 characters in file: {cp437_all_counts}')
             points[SupportedEncoding.CP437] += 1
 
     if DEBUG:
