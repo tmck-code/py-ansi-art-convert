09fed46da12d346f7a5e3f05a130e6f74d1e63f9
Tom McKeesick <tmck01@gmail.com>
2026-03-08 01:42:15 +1100

    add token debug script

---
+++ ops/bin/token-debug.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+#
+# use like:
+#
+# ./ops/bin/token-debug.py tmp/28-SUSHI.ANS -o tmp.out
+
+
+from argparse import ArgumentParser
+from typing import TextIO
+
+from ansi_art_convert import convert
+
+args = {'fpath': 'tmp/28-SUSHI.ANS'}
+
+
+def run(ifpath: str, ostream: TextIO) -> None:
+    print(f'Parsing {ifpath}...')
+    encoding, sauce_extended, data = convert.parse_info({'fpath': ifpath})
+    t = convert.Tokeniser(**(args | {'encoding': encoding, 'sauce': sauce_extended, 'data': data}))
+    r = convert.Renderer(fpath=args['fpath'], tokeniser=t)
+
+    print(f'encoding={encoding}, len(data)={len(data)}')
+
+    convert.set_glyph_offset(0)
+
+    print(f'{encoding=}, {sauce_extended=}, {len(data)=}')
+
+    line: list[convert.ANSIToken] = []
+    nlines = 1
+    for tok in t.tokenise():
+        if isinstance(tok, convert.Color8Token):
+            continue
+        elif isinstance(tok, convert.NewLineToken):
+            print(f'{nlines} - [', file=ostream)
+            for el in line:
+                print(f'    {el!r},', file=ostream)
+            print(']', file=ostream)
+            line.clear()
+            nlines += 1
+        else:
+            line.append(tok)
+
+
+def parse_args() -> dict:
+    parser = ArgumentParser(description='Debug an ANSi file by printing the tokens')
+    parser.add_argument('fpath', help='The path to the ANSi file to debug')
+    parser.add_argument('--output', '-o', help='The path to the output file (default: <input>.debug.ANS)')
+    return parser.parse_args().__dict__
+
+
+def main():
+    args = parse_args()
+    print(f'args={args}')
+    with open(args['output'], 'w') as ostream:
+        run(args['fpath'], ostream)
+
+
+if __name__ == '__main__':
+    main()
